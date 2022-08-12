e61564d63feebf8afc668d0e0f70e0896424ce0b
Gabriel <biel_borel@hotmail.com>
2022-08-12 13:50:23 -0300

    Added open/close CreateTaskModal

--- src/components/CreateTask/CreateTaskButton.tsx
+++ src/components/CreateTask/CreateTaskButton.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { CreateTaskModal } from "./CreateTaskModal";
 
 export const CreateTaskButton = () => {
+  const [isCreateTaskModalOpen, setCreateTaskModalStatus] = useState(false);
+
+  const handleCloseCreateTaskModal = () => setCreateTaskModalStatus(false);
+
   return (
     <>
-      <button className="border-2  w-[110px] hover:bg-blue-400 hover:text-neutral-50 transition-colors rounded-sm text-blue-600 mr-4 border-blue-400 p-2">
+      <button
+        onClick={() => setCreateTaskModalStatus(true)}
+        className="border-2  w-[110px] hover:bg-blue-400 hover:text-neutral-50 transition-colors rounded-sm text-blue-600 mr-4 border-blue-400 p-2"
+      >
         Criar Tarefa
       </button>
 
-      <CreateTaskModal />
+      <CreateTaskModal
+        isVisible={isCreateTaskModalOpen}
+        closeModal={handleCloseCreateTaskModal}
+      />
     </>
   );
 };
