360eb8d87194772f3fbdf20c904f16d9d8e6b3e0
Gabriel <biel_borel@hotmail.com>
2022-08-12 10:41:10 -0300

    Header component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="w-full bg-neutral-50 h-[60px] shadow-md flex items-center justify-center">
+      <span className="text-xl">task</span>
+      <span className="font-bold text-2xl">Calendar</span>
+    </div>
+  );
+};
