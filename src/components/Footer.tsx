a8db12aed440d0423e94aa2de3824e34c1752ff1
Gabriel <biel_borel@hotmail.com>
2022-08-13 20:19:32 -0300

    Created Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <div className="border-t-2 absolute bottom-screen bg-neutral-50 w-full shadow-md text-center py-3">
+      Feito com 💙 por{" "}
+      <a
+        href="https://github.com/gabrielborel"
+        className="text-blue-400 font-semibold text-lg"
+      >
+        Gabriel Borel
+      </a>
+    </div>
+  );
+};
