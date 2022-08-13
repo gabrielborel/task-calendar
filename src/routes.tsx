a14b87a9409a2cbd3ba147da4ad93fa8f17e3363
Gabriel <biel_borel@hotmail.com>
2022-08-13 19:43:28 -0300

    Added a nested route for use of a layout component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import Profile from "./pages/Profile";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
