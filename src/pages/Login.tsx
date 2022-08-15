3bc3fa5406c8615c08b06aab284895624e4a2c48
Gabriel <biel_borel@hotmail.com>
2022-08-15 19:18:38 -0300

    Changed the login page to signIn with Google

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,150 +1,42 @@
-import { useState } from "react";
-import cx from "classnames";
-import { Eye, EyeSlash } from "phosphor-react";
-import * as yup from "yup";
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-const LoginAndSignUpFormSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email("Insira um email válido")
-    .required("Email é obrigatório"),
-  password: yup
-    .string()
-    .required("Senha é obrigatório")
-    .min(6, "No minímo 6 caracteres"),
-});
-
-type Forms = "login" | "signup";
-type Inputs = {
-  email: string;
-  password: string;
-};
+import { jwtDecode } from "../utils/jwtDecode";
 
 export default function Login() {
-  const [selectedForm, setSelectedForm] = useState<Forms>("login");
-  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<Inputs>({
-    resolver: yupResolver(LoginAndSignUpFormSchema),
-  });
 
-  const handleSubmitLogin: SubmitHandler<Inputs> = (data) => {
+  const handleCallbackResponse = (response: any) => {
+    const user = jwtDecode(response.credential);
+    console.log(user);
     navigate("/home");
-    reset();
   };
 
-  const handleSubmitSignup: SubmitHandler<Inputs> = (data) => {
-    navigate("/home");
-    reset();
-  };
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: import.meta.env.VITE__GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInButton") as HTMLElement,
+      {
+        size: "large",
+        theme: "filled_blue",
+        type: "standard",
+      }
+    );
+  }, []);
 
   return (
     <section className="grid place-items-center h-screen">
-      <form
-        onSubmit={
-          selectedForm === "login"
-            ? handleSubmit(handleSubmitLogin)
-            : handleSubmit(handleSubmitSignup)
-        }
-        className="bg-neutral-100 p-4 rounded-md shadow-md w-[350px]"
-      >
-        <header className="flex justify-around items-center">
-          <button
-            type="button"
-            onClick={() => setSelectedForm("login")}
-            className={cx("uppercase font-semibold text-xl py-1 px-3", {
-              "text-blue-500 bg-blue-200 border-b-2 border-b-blue-400 rounded-sm ":
-                selectedForm === "login",
-            })}
-          >
-            login
-          </button>
-          <button
-            type="button"
-            onClick={() => setSelectedForm("signup")}
-            className={cx("uppercase font-semibold text-xl py-1 px-3", {
-              "text-blue-500 bg-blue-200 border-b-2 border-b-blue-400 rounded-sm":
-                selectedForm === "signup",
-            })}
-          >
-            cadastro
-          </button>
-        </header>
-
-        <div className="w-full h-[1px] static bg-neutral-300 my-6 opacity-50" />
-
-        <main className="mt-8">
-          <div>
-            <label htmlFor="title" className="text-lg font-semibold">
-              Email
-            </label>
-            {errors.email && (
-              <span className="block text-sm text-red-500">
-                {errors.email?.message}
-              </span>
-            )}
-            <input
-              {...register("email")}
-              type="email"
-              id="email"
-              placeholder="Insira o seu melhor email"
-              className="block bg-neutral-200 px-3 py-2 rounded-sm w-full"
-            />
-          </div>
-
-          <div className="mt-6">
-            <label htmlFor="title" className="text-lg font-semibold">
-              Senha
-            </label>
-            {errors.password && (
-              <span className="block text-sm text-red-500">
-                {errors.password?.message}
-              </span>
-            )}
-            <div className="flex relative">
-              <input
-                {...register("password")}
-                type={showPassword ? "text" : "password"}
-                id="password"
-                placeholder="Insira a sua senha"
-                className="block bg-neutral-200 px-3 py-2 rounded-sm w-full"
-              />
-              {showPassword ? (
-                <Eye
-                  size={18}
-                  weight="bold"
-                  className="cursor-pointer absolute right-3 top-3"
-                  onClick={() => setShowPassword(false)}
-                />
-              ) : (
-                <EyeSlash
-                  size={18}
-                  weight="bold"
-                  className="cursor-pointer absolute opacity-60 right-3 top-3"
-                  onClick={() => setShowPassword(true)}
-                />
-              )}
-            </div>
-          </div>
-        </main>
-
-        <footer className="flex items-center justify-center mt-6">
-          <button
-            type="submit"
-            className=" bg-blue-500 text-neutral-50 transition-colors rounded-sm py-3 w-[70%] font-semibold uppercase text-xl"
-          >
-            {selectedForm === "login" ? "entrar" : "criar conta"}
-          </button>
-        </footer>
-      </form>
+      <div className="bg-neutral-100 rounded-md shadow-md p-4 text-center">
+        <div className="text-center mb-4">
+          <span className="text-3xl">task</span>
+          <span className="font-bold text-4xl">Calendar</span>
+        </div>
+        <p className="font-semibold text-2xl">Entre com sua conta Google</p>
+        <div className="mt-4 mx-auto w-fit" id="signInButton" />
+      </div>
     </section>
   );
 }
